ebc5ca43f3e3f171c7fae9aa6eead9912432e39c
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-11-05 11:17:51 -0800

    chore(lexer): add setInput && addRule methods
    Add option to instantiate lexer class with an input parameter.

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,29 +1,60 @@
-export interface IRule {
-    id: string;
-    match: string;
-}
+import Rule, { IRule } from './rule';
 
 /**
  * @class `Lexer`
  */
 export default class Lexer {
-    public tokens: string[];
-    public col: number;
-    public rules: IRule[];
+    public column: number;
     public input: string;
     public line: number;
+    public rules: IRule[];
+    public tokens: object[];
 
     private index: number;
-    private expression: string;
-    private regex?: RegExp;
 
-    constructor() {
-        this.col = 1;
-        this.input = '';
+    constructor(input?: string) {
+        this.column = 1;
+        this.input = input || '';
         this.line = 1;
         this.index = 0;
-        this.expression = '';
-        this.tokens = [];
         this.rules = [];
+        this.tokens = [];
+    }
+
+    /**
+     *
+     * @name addRule
+     * @param {RegExp} regex
+     * @param {Function} action
+     * @param {Array<number>} begin
+     * @returns {Lexer}
+     */
+    public addRule(type: string, regex: RegExp | string, action?: () => any, begin?: number[]) {
+
+        // If begin parameter not given, set it to [0](active rule);
+        if (!begin) {
+            begin = [0];
+        }
+
+        // If regex given is a string, strip the quotes before instantiating a new `Rule`
+        if (typeof regex === 'string') {
+            regex.replace(/"|'/g, '');
+        }
+
+        const rule = new Rule(type, regex, action, begin);
+        this.rules.push(rule);
+
+        return this;
+    }
+
+    /**
+     * @name setInput
+     * @param {String} input
+     * @returns {Lexer}
+     */
+    public setInput(input: string) {
+        this.input += input;
+
+        return this;
     }
 }
