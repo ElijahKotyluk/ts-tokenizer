05447afc067ead6689d4ae2ae39db65fa308bbea
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-11-07 23:44:33 -0800

    chore(jest): update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  preset: "ts-jest",
-  roots: ["tests/"],
-  testMatch: ["**/tests/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-  testPathIgnorePatterns: ["/node_modules/"]
+  preset: 'ts-jest',
+  roots: ['tests/'],
+  testMatch: ['**/tests/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+  testPathIgnorePatterns: ['/node_modules/'],
 };
