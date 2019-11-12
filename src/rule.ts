909493f91ae61a1bdaf574598feeae2d5ef9f099
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-11-11 17:18:50 -0800

    chore(rule): update rule class

--- src/rule.ts
+++ src/rule.ts
@@ -2,19 +2,19 @@ export interface IRule {
     type: string;
     regex: RegExp;
     fn?: () => any;
-    begin: number[];
+    begin?: number[];
 }
 
 export default class Rule implements IRule {
     public type: string;
     public regex: RegExp;
     public fn?: () => any;
-    public begin: number[];
+    public begin?: number[];
 
     constructor(type: string, regex: RegExp, fn?: () => any, begin?: number[]) {
         this.type = type;
         this.regex = regex;
         this.fn = fn;
-        this.begin = [] || begin;
+        this.begin = begin || [0];
     }
 }
