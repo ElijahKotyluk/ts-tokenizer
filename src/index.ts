1587a2460feada5ba3d54b11657e4a62161e3fbe
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-11-16 12:30:56 -0800

    chore(index): update exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import Lexer, { ILexer } from './lexer';
-
-export { ILexer, Lexer };
+export { ILexer, default as Lexer } from './lexer';
+export { IRule, default as Rule } from './rule';
+export { IState, default as State } from "./state";
+export { IToken, default as Token } from './token';
