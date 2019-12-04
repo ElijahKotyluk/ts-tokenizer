8e34bed1b44246a444b799da99ac2ef67ca378e3
Elijah Kotyluk <elijah@elijahkotyluk.com>
2019-12-03 16:09:33 -0800

    chore(pkg): update pkg

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import Lexer from './lexer';
 
-export { ILexer } from './lexer';
+export { ILexer, default as Lexer } from './lexer';
 export { IRule, default as Rule } from './rule';
 export { IState, default as State } from "./state";
 export { IToken, default as Token } from './token';
-
-module.exports = Lexer;
