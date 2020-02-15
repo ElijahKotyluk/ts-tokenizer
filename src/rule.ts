6505332fca7e2996d44e5f3273e35d80d7b9ff6a
Elijah Kotyluk <elijah@elijahkotyluk.com>
2020-02-14 21:31:44 -0800

    chore(rules): remove begin prop/condition

--- src/rule.ts
+++ src/rule.ts
@@ -2,19 +2,16 @@ export interface IRule {
     type: string;
     regex: RegExp;
     fn?: () => any;
-    begin?: number[];
 }
 
 export default class Rule implements IRule {
     public type: string;
     public regex: RegExp;
     public fn?: () => any;
-    public begin?: number[];
 
-    constructor(type: string, regex: RegExp, fn?: () => any, begin?: number[]) {
+    constructor(type: string, regex: RegExp, fn?: () => any) {
         this.type = type;
         this.regex = regex;
         this.fn = fn;
-        this.begin = begin || [0];
     }
 }
